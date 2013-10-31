638b6ed6ed77fd21de88611063adc32ebaa8b9f9
Johan Sageryd <j@1616.se>
2013-10-31 15:43:11 +0100

    Fix timelog scripts

--- scripts/timelog/tl.bash
+++ scripts/timelog/tl.bash
@@ -23,10 +23,10 @@ else
       count=0
       clear
     fi
-    cut -d ' ' -f 1-3,5- "$LEDGER_TIMELOG" | ledger -f - $options $@
+    cut -d ' ' -f 1-3,5- "$LEDGER_TIMELOG" | ledger -f - $options "$@"
     if [ $? -eq 0 ]; then
       last=$(tail -1 "$LEDGER_TIMELOG")
-      if [ ${last:0:1} = 'i' ]; then
+      if [ "${last:0:1}" = 'i' ]; then
         running="\033[1;32m[RUNNING]\033[0m"
         task=$(echo "$last" | sed -E "s/^. +[^ ]+ +[^ ]+ +[^ ]+ +(.+)  +([^ ]+)$/\2 $(printf "\033[1;34m")(\1)$(printf "\033[0m")/")
         status="$running $task"
