225ec954188f615fe804dc1951b1e5ab7ed205d9
Johan Sageryd <j@1616.se>
2014-06-13 13:12:26 +0200

    Always fetch, merge only when timer not running

--- scripts/timelog/tu.bash
+++ scripts/timelog/tu.bash
@@ -7,6 +7,10 @@ if [ -z "$LEDGER_TIMELOG" ]; then
   exit 1
 fi
 
+echo 'Fetching...'
+git --git-dir="$(dirname $LEDGER_TIMELOG)/.git" fetch
+echo 'done.'
+
 last=$(tail -1 "$LEDGER_TIMELOG")
 if [ "${last:0:1}" = 'i' ]; then
   echo 'Log seems to be running. Clock out before update.' >&2
@@ -14,7 +18,7 @@ if [ "${last:0:1}" = 'i' ]; then
 fi
 
 pushd $(dirname "$LEDGER_TIMELOG") && \
-git pull --ff-only && \
+git merge @{u} --ff-only && \
 git add $(basename "$LEDGER_TIMELOG") && \
 git commit -m "Update $(date +'%Y-%m-%d %H:%M')" && \
 git push && \
