c7bee49d8963993621911e6d836dfe66c22da53a
Johan Sageryd <j@1616.se>
2014-08-15 21:59:13 +0200

    Add script to check if txns are chronological

---
+++ scripts/ledger/txns_chronological.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+unsorted=$(ledger register --format='%(format_date(date, "%Y-%m-%d"))\n' | uniq)
+if diff <(echo "$unsorted") <(echo "$unsorted" | sort); then
+  echo -e "\033[1;32mTransactions are in chronological order.\033[0m"
+else
+  echo -e "\n\033[1;31mTransactions are not in chronological order.\033[0m"
+fi
