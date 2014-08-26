f80af5ce86c48cadd6c24de7285555749a745938
Johan Sageryd <j@1616.se>
2014-08-26 11:17:20 +0200

    Add ramdisk script

---
+++ scripts/ramdisk/ramdisk.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# Creates a ramdisk on OS X
+
+size=$1
+
+if [ -z $size ]; then
+  echo 'Usage: ramdisk <size in gb>; e.g. ramdisk 2 to create a 2 GB ramdisk'
+  exit 1
+fi
+
+mb=$(($size * 1024))
+blocks=$(($mb * 2048))
+
+diskutil erasevolume HFS+ "ramdisk_$mb" `hdiutil attach -nomount ram://$blocks`
