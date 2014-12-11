8ee3047939b7d9c2bcece45303f564beb3b1733a
Johan Sageryd <j@1616.se>
2014-12-11 20:24:58 +0100

    Remove .rspec
    Don't need it anymore since our rspec output became sane.

--- init.bash
+++ init.bash
@@ -31,7 +31,6 @@ _link ${CONFIG_ROOT}/ledger/.ledgerrc ${HOME}/.ledgerrc
 
 _link ${CONFIG_ROOT}/profile/.bash_profile ${HOME}/.bash_profile
 _link ${CONFIG_ROOT}/psql/.psqlrc ${HOME}/.psqlrc
-_link ${CONFIG_ROOT}/rspec/.rspec ${HOME}/.rspec
 _link ${CONFIG_ROOT}/taskwarrior/.taskrc ${HOME}/.taskrc
 _link ${CONFIG_ROOT}/tmux/.tmux.conf ${HOME}/.tmux.conf
 _link ${CONFIG_ROOT}/vim/.vimrc ${HOME}/.vimrc
