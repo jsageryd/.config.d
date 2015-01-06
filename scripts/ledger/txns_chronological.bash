9a4ca0a1bd707c43484eef9db126b9d05b4d7838
Johan Sageryd <j@1616.se>
2015-01-06 13:52:46 +0100

    Return proper exit code on error
    And let's explicitly exit 0 when there's no error for clarity.

--- scripts/ledger/txns_chronological.bash
+++ scripts/ledger/txns_chronological.bash
@@ -3,6 +3,8 @@
 unsorted=$(ledger register --format='%(format_date(date, "%Y-%m-%d"))\n' | uniq)
 if diff <(echo "$unsorted") <(echo "$unsorted" | sort); then
   echo -e "\033[1;32mTransactions are in chronological order.\033[0m"
+  exit 0
 else
   echo -e "\n\033[1;31mTransactions are not in chronological order.\033[0m"
+  exit 1
 fi
