8f17da776c90e77a38c5ab8643075a8d65987716
Johan Sageryd <j@1616.se>
2015-01-24 21:58:05 +0100

    Update tmux-dev
    Make it faster to get to the dev layout I mostly use.

--- scripts/tmux/tmux-dev.sh
+++ scripts/tmux/tmux-dev.sh
@@ -4,6 +4,9 @@ tmux new-session -d
 tmux split-window -v
 tmux split-window -v
 tmux split-window -h
+tmux split-window -h
+tmux resize-pane -x 120 -y 16 -t 2
+tmux resize-pane -x 120 -t 3
 tmux select-pane -t 0
 tmux split-window -h
 tmux split-window -h
@@ -12,4 +15,13 @@ tmux resize-pane -x 100 -t 2
 tmux select-pane -t 3
 tmux split-window -h -p 45
 tmux select-pane -t 0
+tmux send-keys -t 0 'tl loop r -p today @bg' Enter
+tmux send-keys -t 1 'cd ~/Documents/ledger/time/ ; clear' Enter
+tmux send-keys -t 2 'cd ~/Documents/ledger/personal/ ; clear' Enter
+tmux send-keys -t 3 'cd ~/Documents/bonnier/sparkle/ ; clear' Enter
+tmux send-keys -t 4 'cd ~/Documents/bonnier/sparkle/ ; clear' Enter
+tmux send-keys -t 5 'cd ~/Documents/bonnier/sparkle-deploy/ ; clear' Enter
+tmux send-keys -t 6 'cd ~/Documents/bonnier/sparkle-deploy/ ; clear' Enter
+tmux send-keys -t 7 'cd ~/Documents/bonnier/sparkle-deploy/ ; clear' Enter
+tmux select-pane -t 3
 tmux attach
