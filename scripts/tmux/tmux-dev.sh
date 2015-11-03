dffd61b4e2696b483cbd320af7d4bca1f8b72371
Johan Sageryd <j@1616.se>
2015-11-03 11:29:34 +0100

    Simplify tmux-dev setup
    I work on a 12" screen now so it doesn't make sense to have all those
    panes; instead let's have just the time log at the top and a
    use-for-anything empty pane at the bottom.

--- scripts/tmux/tmux-dev.sh
+++ scripts/tmux/tmux-dev.sh
@@ -1,27 +1,8 @@
 #!/usr/bin/env sh
 
 tmux new-session -d
-tmux split-window -v
-tmux split-window -v
+tmux split-window -bv -l 16
 tmux split-window -h
-tmux split-window -h
-tmux resize-pane -x 120 -y 16 -t 2
-tmux resize-pane -x 120 -t 3
-tmux select-pane -t 0
-tmux split-window -h
-tmux split-window -h
-tmux resize-pane -x 66 -y 20 -t 0
-tmux resize-pane -x 100 -t 2
-tmux select-pane -t 3
-tmux split-window -h -p 45
-tmux select-pane -t 0
-tmux send-keys -t 0 'tl loop r -p today @bg' Enter
-tmux send-keys -t 1 'cd ~/Documents/ledger/time/ ; clear' Enter
-tmux send-keys -t 2 'cd ~/Documents/ledger/personal/ ; clear' Enter
-tmux send-keys -t 3 'cd ~/Documents/bonnier/sparkle/ ; clear' Enter
-tmux send-keys -t 4 'cd ~/Documents/bonnier/sparkle/ ; clear' Enter
-tmux send-keys -t 5 'cd ~/Documents/bonnier/sparkle-deploy/ ; clear' Enter
-tmux send-keys -t 6 'cd ~/Documents/bonnier/sparkle-deploy/ ; clear' Enter
-tmux send-keys -t 7 'cd ~/Documents/bonnier/sparkle-deploy/ ; clear' Enter
-tmux select-pane -t 3
+tmux send-keys -t 2 'tl loop r -p today @eg' Enter
+tmux select-pane -t 1
 tmux attach
