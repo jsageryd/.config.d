0c40c2d7dc73f002ae8f25d77ade03ffb184d45e
Johan Sageryd <j@1616.se>
2015-11-04 11:23:21 +0100

    Teach txns_chronological to override $LEDGER_FILE
    $ txns_chronological      # ledger
    $ txns_chronological foo  # LEDGER_FILE=foo ledger

--- scripts/ledger/txns_chronological.bash
+++ scripts/ledger/txns_chronological.bash
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-unsorted=$(ledger register --format='%(format_date(date, "%Y-%m-%d"))\n' | uniq)
+if ! test -z "$1"; then
+  ledger_file=$1
+fi
+
+unsorted=$(LEDGER_FILE="$ledger_file" ledger register --format='%(format_date(date, "%Y-%m-%d"))\n' | uniq)
+echo "$LEDGER_FILE"
 if diff <(echo "$unsorted") <(echo "$unsorted" | sort); then
   echo -e "\033[1;32mTransactions are in chronological order.\033[0m"
   exit 0
