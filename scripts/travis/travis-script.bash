114c92b5eed2007411aa1b9689f0648f00b0e75d
Johan Sageryd <j@1616.se>
2016-11-04 12:26:51 +0100

    Make travis-script install its dependencies
    So that I don't have to look for them manually.

--- scripts/travis/travis-script.bash
+++ scripts/travis/travis-script.bash
@@ -1,7 +1,32 @@
 #!/usr/bin/env bash
 
-if [ -f ".travis.yml" ]; then
-  yaml2json .travis.yml | jq -r ".script|select (.!=null)|.[]" | sh
-else
+if [ ! -f ".travis.yml" ]; then
   echo ".travis.yml missing"
+  exit 1
 fi
+
+if ! which -s yaml2json; then
+  cmd="go get github.com/buildkite/yaml2json"
+  echo "yaml2json not found."
+  read -p "$cmd [y/n]? " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    $cmd
+  else
+    exit 0
+  fi
+fi
+
+if ! which -s jq; then
+  cmd="brew install jq"
+  echo "jq not found."
+  read -p "$cmd [y/n]? " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    $cmd
+  else
+    exit 0
+  fi
+fi
+
+yaml2json .travis.yml | jq -r ".script|select (.!=null)|.[]" | sh
