54a4e18d24d39136c2fce3971114f79c580a47ca
Johan Sageryd <j@1616.se>
2016-12-05 10:50:47 +0100

    Have git-ch copy 12-char hashes instead of 10
    Let's move to 12 chars instead of 10; 12 is just two more.
    
    Linus says:
      "I tend to use the 12-character short version in commit messages"
    
    https://lkml.org/lkml/2010/10/14/358

--- scripts/git/workflow/ch.bash
+++ scripts/git/workflow/ch.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-hash=$(git rev-parse --verify --short=10 ${1:-HEAD} | tr -d '\n')
+hash=$(git rev-parse --verify --short=12 ${1:-HEAD} | tr -d '\n')
 printf $hash | pbcopy;
 printf "Hash copied: $(git log -1 --oneline --color $hash)\n"
