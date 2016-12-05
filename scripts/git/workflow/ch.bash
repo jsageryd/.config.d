5d816599e3b00e0d8cdffd57f62db8a4b21d0e05
Johan Sageryd <j@1616.se>
2016-12-05 11:11:25 +0100

    Use same hash length as copied hash in info output

--- scripts/git/workflow/ch.bash
+++ scripts/git/workflow/ch.bash
@@ -2,4 +2,4 @@
 
 hash=$(git rev-parse --verify --short=12 ${1:-HEAD} | tr -d '\n')
 printf $hash | pbcopy;
-printf "Hash copied: $(git log -1 --oneline --color $hash)\n"
+printf "Hash copied: $(git log -1 --abbrev=12 --oneline --color $hash)\n"
