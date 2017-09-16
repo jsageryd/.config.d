672dd468e4131de9d54c0d4f2e5e8628a3d46fad
Johan Sageryd <j@1616.se>
2017-09-16 16:45:41 +0200

    Use 'echo -en' instead of printf
    To avoid printf interpreting the string to print as a format string,
    which it does for e.g. the git-prompt branch info string "master %".

--- scripts/iterm2/status.bash
+++ scripts/iterm2/status.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [ -n $TMUX ]; then
-  printf "\033Ptmux;\033"
-  printf "\033]1337;SetKeyLabel=status=$*\a"
-  printf "\033\\"
+  echo -en "\033Ptmux;\033"
+  echo -en "\033]1337;SetKeyLabel=status=$*\a"
+  echo -en "\033\\"
 else
-  printf "\033]1337;SetKeyLabel=status=$*\a"
+  echo -en "\033]1337;SetKeyLabel=status=$*\a"
 fi
