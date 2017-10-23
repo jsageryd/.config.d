2eacac8764504cf56390d87e9dc9cfef7fc15087
Johan Sageryd <j@1616.se>
2017-10-23 13:30:55 +0200

    Update rediscloud-cli to take Heroku app name
    Before:
      $ heroku config:get REDISCLOUD_URL --app <heroku app>
      (copy-paste etc.)
      $ rediscloud-cli redis://...
    
    After:
      $ rediscloud-cli <heroku app>

--- scripts/redis/rediscloud-cli.bash
+++ scripts/redis/rediscloud-cli.bash
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
-# Takes an URL like redis://rediscloud:<password>@<host>:<port>
-# and runs redis-cli with extracted password, host, password.
+# Takes a Heroku application name and runs redis-cli with password, host,
+# password extracted from the REDISCLOUD_URL env variable in the application
+# settings.
 
 if [ -z $1 ]; then
-  >&2 echo "Usage: rediscloud-cli redis://rediscloud:<password>@<host>:<port>"
+  >&2 echo "Usage: rediscloud-cli <heroku application name>"
   exit 1
 fi
 
-password=$(sed 's#.*:\(.*\)@.*#\1#' <<< $1)
-host=$(sed 's#.*@\(.*\):.*#\1#' <<< $1)
-port=$(sed 's#.*:\(.*\).*#\1#' <<< $1)
+url=$(heroku config:get REDISCLOUD_URL --app $1)
+
+password=$(sed 's#.*:\(.*\)@.*#\1#' <<< $url)
+host=$(sed 's#.*@\(.*\):.*#\1#' <<< $url)
+port=$(sed 's#.*:\(.*\).*#\1#' <<< $url)
 
 redis-cli -h $host -p $port -a $password
