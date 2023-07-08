464ded8e6be642bbf95c99610064e59d1a7b9492
Johan Sageryd <j@1616.se>
2023-07-08 17:53:30 +0200

    git: Remove status info from touch bar display
    I don't have touch bar anymore.

--- scripts/iterm2/status.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! -z $TMUX ]; then
-  echo -en "\033Ptmux;\033"
-  echo -en "\033]1337;SetKeyLabel=status=$*\a"
-  echo -en "\033\\"
-else
-  echo -en "\033]1337;SetKeyLabel=status=$*\a"
-fi
