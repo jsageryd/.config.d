7b09fa60143f0ddfb6c9d30efd0cd2e9a43359ab
Johan Sageryd <j@1616.se>
2024-01-27 16:37:30 +0100

    Add script to share files using x0.at

---
+++ scripts/x0/x0.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# This script uploads the given file(s) to x0.at and prints the returned URL(s).
+
+if test -z "$1"; then
+  echo 'Usage: x0 <file> [<file> ...]'
+  exit 1
+fi
+
+for file in "$@"; do
+  if test ! -f "$file"; then
+    printf 'Not a regular file: \e[38;5;190m%s\e[0m\n' "$file"
+    exit 1
+  fi
+  printf '\e[38;5;190m%s\e[0m\n' "$file"
+done
+
+read -r -n1 -p "Share to x0.at [y/n]? " answer
+echo
+if test "$answer" = 'y' -o "$answer" = 'Y'; then
+  for file in "$@"; do
+    url=$(curl -sF file=@"$file" https://x0.at/)
+    printf '\e[38;5;190m%s\e[0m\n' $url
+  done
+fi
