1cb14d2cbd5371bfd660c5c11b402ba17273dffd
Johan Sageryd <j@1616.se>
2024-12-10 23:09:09 +0100

    git: Have git-ch return 16 character commit hashes
    To match the default abbreviation length I set in the previous commit.

--- scripts/git/ch.bash
+++ scripts/git/ch.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-hash=$(git rev-parse --verify --short=12 ${1:-HEAD} | tr -d '\n')
+hash=$(git rev-parse --verify --short=16 ${1:-HEAD} | tr -d '\n')
 printf $hash | pbcopy;
-printf "Hash copied: $(git log -1 --abbrev=12 --oneline --color $hash)\n"
+printf "Hash copied: $(git log -1 --abbrev=16 --oneline --color $hash)\n"
