d7caab5cc4a46d458b6cee6f77d9399acea43671
Johan Sageryd <j@1616.se>
2025-06-10 22:47:56 +0200

    Adapt everything to support light colours
    In bright environments I sometimes find it difficult to read the screen
    because of reflections off of the dark terminal background. This commit
    tweaks everything (ag, git, prompt, scripts, tig, tmux, vim) to display
    decently in light mode.

--- scripts/apikey/apikey.sh
+++ scripts/apikey/apikey.sh
@@ -2,4 +2,8 @@
 
 key=$(</dev/random head -c 2048 | base64 | tr -d '+/=' | head -c 20)
 printf $key | pbcopy
-printf 'New key copied: \e[38;5;45m%.3s...\e[0m\n' $key
+if [ "$COLOUR_SCHEME" = "light" ]; then
+  printf 'New key copied: \e[48;5;45m%.3s...\e[0m\n' $key
+else
+  printf 'New key copied: \e[38;5;45m%.3s...\e[0m\n' $key
+fi
