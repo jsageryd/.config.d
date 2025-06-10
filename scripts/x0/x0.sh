661c4368db10001f5f49ad568839e31b8945c201
Johan Sageryd <j@1616.se>
2025-06-10 22:47:56 +0200

    Adapt everything to support light colours
    In bright environments I sometimes find it difficult to read the screen
    because of reflections off of the dark terminal background. This commit
    tweaks everything (ag, git, prompt, scripts, tig, tmux, vim) to display
    decently in light mode.

--- scripts/x0/x0.sh
+++ scripts/x0/x0.sh
@@ -2,6 +2,12 @@
 
 # This script uploads the given file(s) to x0.at and prints the returned URL(s).
 
+if [ "$COLOUR_SCHEME" = "light" ]; then
+  hl="\e[48;5;190m%s\e[0m"
+else
+  hl="\e[38;5;190m%s\e[0m"
+fi
+
 if test -z "$1"; then
   echo 'Usage: x0 <file> [<file> ...]'
   exit 1
@@ -9,10 +15,10 @@ fi
 
 for file in "$@"; do
   if test ! -f "$file"; then
-    printf 'Not a regular file: \e[38;5;190m%s\e[0m\n' "$file"
+    printf "Not a regular file: $hl\n" "$file"
     exit 1
   fi
-  printf '\e[38;5;190m%s\e[0m\n' "$file"
+  printf "$hl\n" "$file"
 done
 
 read -r -n1 -p "Share to x0.at [y/n]? " answer
@@ -20,7 +26,7 @@ echo
 if test "$answer" = 'y' -o "$answer" = 'Y'; then
   for file in "$@"; do
     url=$(curl -sF file=@"$file" https://x0.at/)
-    printf '\e[38;5;190m%s\e[0m\n' $url >&2
+    printf "$hl\n" $url >&2
     echo "$url"
   done | paste -s -d ' ' - | tr -d '\n' | pbcopy &&
   printf '%d URL(s) copied to clipboard\n' $#
