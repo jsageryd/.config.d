5750c9ef2fbdd4b700aa268477da1c532bcba9bd
Johan Sageryd <j@1616.se>
2026-04-02 13:27:09 +0200

    nvim: Add nvim-goc.lua
    Provides Go test coverage info.
    
    Source: https://github.com/rafaelsq/nvim-goc.lua
    Commit: 76612147223ef92c6bb2905c4228b7d5617baefd

---
+++ nvim/pack/plugins/start/nvim-goc.lua/README.md
@@ -0,0 +1,62 @@
+# nvim-goc.lua
+easy go coverage
+
+![image](https://user-images.githubusercontent.com/1598854/131515315-6178a680-cad1-4ccb-90e4-c61245f10b67.png)
+
+## Setup
+
+```lua
+
+-- if set, when we switch between buffers, it will not split more than once. It will switch to the existing buffer instead
+vim.opt.switchbuf = 'useopen'
+
+local goc = require'nvim-goc'
+goc.setup({ verticalSplit = false })  -- default to horizontal
+
+
+vim.keymap.set('n', '<Leader>gcf', goc.Coverage, {silent=true})       -- run for the whole File
+vim.keymap.set('n', '<Leader>gct', goc.CoverageFunc, {silent=true})   -- run only for a specific Test unit
+vim.keymap.set('n', '<Leader>gcc', goc.ClearCoverage, {silent=true})  -- clear coverage highlights
+
+-- If you need custom arguments, you can supply an array as in the example below.
+-- vim.keymap.set('n', '<Leader>gcf', function() goc.Coverage({ "-race", "-count=1" }) end, {silent=true})
+-- vim.keymap.set('n', '<Leader>gct', function() goc.CoverageFunc({ "-race", "-count=1" }) end, {silent=true})
+
+vim.keymap.set('n', ']a', goc.Alternate, {silent=true})
+vim.keymap.set('n', '[a', goc.AlternateSplit, {silent=true})          -- set verticalSplit=true for vertical
+
+-- If you prefer, you can also use AlternateFile to work with other languages:
+-- 1. create an augroup for the file type
+-- 2. goc.AlternateFile(split: bool, file_suffix: str, which_cmd_to_split: [vert|to])
+-- javascript example:
+vim.keymap.set('n', ']a', function() goc.AlternateFile(false, '.test', 'vert') end, { silent = true })
+vim.keymap.set('n', '[a', function() goc.AlternateFile(true, '.test', 'vert') end, { silent = true })
+
+-- If you want to open it in your browser, you can use the commands below.
+cf = function(testCurrentFunction)
+  local cb = function(path, index)
+    if path then
+
+      -- `xdg-open|open` command performs the same function as double-clicking on the file.
+      -- change from `xdg-open` to `open` on MacOSx
+      vim.cmd(":silent exec \"!xdg-open file://" .. path .. "\\\\#file" .. index .. "\"")
+    end
+  end
+
+  if testCurrentFunction then
+    goc.CoverageFunc(nil, cb, 0)
+  else
+    goc.Coverage(nil, cb)
+  end
+end
+
+-- You need to create a callback function to configure which command to use to open the HTML.
+-- On Linux, `xdg-open` is generally used, on MacOSx it's just `open`.
+vim.keymap.set('n', '<leader>gca', cf, {silent=true})
+vim.keymap.set('n', '<Leader>gcb', function() cf(true) end, {silent=true})
+
+-- default colors
+-- vim.api.nvim_set_hl(0, 'GocNormal', {link='Comment'})
+-- vim.api.nvim_set_hl(0, 'GocCovered', {link='String'})
+-- vim.api.nvim_set_hl(0, 'GocUncovered', {link='Error'})
+```
---
+++ nvim/pack/plugins/start/nvim-goc.lua/lua/nvim-goc.lua
@@ -0,0 +1,245 @@
+local ts = vim.treesitter
+
+local M = {
+  hi = vim.api.nvim_create_namespace("goc"),
+  errBuf = nil,
+  splitCmd = 'sp ',
+  splitSBCmd = 'to ',
+}
+
+M.setup = function(opts)
+  vim.api.nvim_set_hl(0, 'GocNormal', {link='Comment'})
+  vim.api.nvim_set_hl(0, 'GocCovered', {link='String'})
+  vim.api.nvim_set_hl(0, 'GocUncovered', {link='Error'})
+
+  if opts then
+      local verticalSplit = opts.verticalSplit or false
+      assert(type(verticalSplit) == "boolean", "verticalSplit must be boolean or nil")
+      M.splitCmd = verticalSplit and 'vsp ' or 'sp '
+      M.splitSBCmd = verticalSplit and 'vert ' or 'to '
+  end
+end
+
+M.Coverage = function(fn, html, customArgs)
+  print('[goc] ...')
+  if M.errBuf ~= nil then
+    vim.api.nvim_buf_set_lines(M.errBuf, 0, -1, false, {"..."})
+  end
+  local fullPathFile = string.gsub(vim.api.nvim_buf_get_name(0), "_test", "")
+  local bufnr = vim.uri_to_bufnr("file://" .. fullPathFile)
+
+  -- use '%:.' to ensure the current path is relative (required for matching
+  -- coverage output)
+  local relativeFile = string.gsub(vim.fn.expand('%:.'), "_test", "")
+  local package = vim.fn.expand('%:p:h')
+  local tmp = vim.api.nvim_eval('tempname()')
+
+  local h = nil
+
+  local args = {'test', '-coverprofile', tmp, package}
+  if fn then
+    args = {'test', '-coverprofile', tmp, "-run", fn, package}
+  end
+
+  if customArgs ~= nil then
+    for i=1, #customArgs do
+      table.insert(args, 1+i, customArgs[i])
+    end
+  end
+
+  if M.errBuf == nil then
+    M.errBuf = vim.api.nvim_create_buf(true, true)
+  end
+
+  local stdout = vim.loop.new_pipe(false)
+  local stderr = vim.loop.new_pipe(false)
+  h = vim.loop.spawn('go', {args = args, stdio = {nil, stdout, stderr}}, vim.schedule_wrap(function(code, signal)
+    M.ClearCoverage(bufnr)
+
+    stdout:read_stop()
+    stderr:read_stop()
+    h:close()
+
+    if code == 0 then
+      local percent = string.gmatch(table.concat(vim.api.nvim_buf_get_lines(M.errBuf, 0, -1, true)), 'coverage: (%d+)')()
+      if percent ~= nil then
+        print('[goc] coverage', percent .. '%')
+        if #vim.fn.win_findbuf(M.errBuf) > 0 then
+          vim.api.nvim_buf_delete(M.errBuf, {force=true})
+          M.errBuf = nil
+        end
+      else
+        print("[goc] check output!")
+        if #vim.fn.win_findbuf(M.errBuf) == 0 then
+          vim.cmd("vert sb " .. M.errBuf)
+        end
+      end
+      if html then
+        local lines = vim.api.nvim_eval('readfile("' .. tmp .. '")')
+        local file_i = -1
+        local last_file = ''
+        local final = ''
+        for i = 2,#lines do
+          local path = string.gmatch(lines[i], '(.+):')()
+          if last_file ~= path and final == '' then
+            last_file = path
+            file_i = file_i + 1
+          end
+
+          -- For every line in the coverage output, look for the current relative
+          -- 'path/to/foo.go' (and not /abs/path/to/foo.go or ./path/to/foo.go).
+          -- This must use 'vim.fn.expand("%:.")' when calculating relativeFile
+          -- for this to work.
+          if path:sub(-#relativeFile) == relativeFile then
+            final = last_file
+            break
+          end
+        end
+        local tmphtml = vim.api.nvim_eval('tempname()') .. '.html'
+        vim.cmd(':silent exec "!go tool cover -html='.. tmp ..' -o '.. tmphtml ..'"')
+        html(tmphtml, file_i)
+        return
+      end
+
+      if not vim.api.nvim_buf_is_loaded(bufnr) or #vim.fn.win_findbuf(bufnr) == 0 then
+        vim.cmd(M.splitCmd .. string.gsub(fullPathFile, vim.fn.getcwd() .. '/', ''))
+      elseif vim.tbl_contains(vim.opt.switchbuf:get(), 'useopen') then
+        vim.cmd(":sb " .. string.gsub(fullPathFile, vim.fn.getcwd() .. '/', ''))
+      end
+
+      for i = 0, vim.fn.line("$") do
+        local buflines = vim.api.nvim_buf_get_lines(bufnr, i, i + 1, false)
+        local line = buflines[1] or ""
+        vim.api.nvim_buf_set_extmark(bufnr, M.hi, i, 0, {
+          end_row = i,
+          end_col = #line,
+          hl_group = "GocNormal",
+          priority = 130,
+        })
+      end
+
+      local lines = vim.api.nvim_eval('readfile("' .. tmp .. '")')
+      for i = 2,#lines do
+        local path = string.gmatch(lines[i], '(.+):')()
+
+        -- For every line in the coverage output, look for the current relative
+        -- 'path/to/foo.go' (and not /abs/path/to/foo.go or ./path/to/foo.go).
+        -- This must use 'vim.fn.expand("%:.")' when calculating relativeFile
+        -- for this to work.
+        if path:sub(-#relativeFile) == relativeFile then
+
+          local marks = string.gmatch(string.gsub(lines[i], '[^:]+:', ''), '%d+')
+
+          local startline = math.max(tonumber(marks()) - 1, 0)
+          local startcol = math.max(tonumber(marks()) - 1, 0)
+          local endline = math.max(tonumber(marks()) -1, 0)
+          local endcol = tonumber(marks())
+          local numstmt = tonumber(marks())
+          local cnt = tonumber(marks())
+
+          local hig = "GocUncovered"
+          if cnt == 1 then
+            hig = "GocCovered"
+          end
+
+          for y = startline, endline do
+            local sc = 0
+            local opts = {
+              end_row = y,
+              priority = 131,
+              hl_group = hig,
+            }
+            if startline == y then
+              sc = startcol
+            end
+            if endline == y then
+              opts.end_col = endcol - 1
+            else
+              local buflines = vim.api.nvim_buf_get_lines(bufnr, y, y + 1, false)
+              local line = buflines[1] or ""
+              opts.end_col = #line
+            end
+
+            vim.api.nvim_buf_set_extmark(bufnr, M.hi, y, sc, opts)
+          end
+        end
+      end
+    else
+      print("[goc] fail!")
+      if #vim.fn.win_findbuf(M.errBuf) == 0 then
+        vim.cmd(M.splitSBCmd .. " sb " .. M.errBuf)
+      end
+    end
+  end))
+
+  local writeToScratch = function(err, data)
+    vim.schedule(function()
+      if err then
+        vim.api.nvim_buf_set_lines(M.errBuf, -1, -1, false, vim.split(err, "\n"))
+        return
+      end
+      if data then
+        vim.api.nvim_buf_set_lines(M.errBuf, -1, -1, false, vim.split(data, "\n"))
+      end
+    end)
+  end
+
+  vim.loop.read_start(stdout, writeToScratch)
+  vim.loop.read_start(stderr, writeToScratch)
+end
+
+M.CoverageFunc = function(p, html, customArgs)
+  if not p then
+    p = ts.get_node()
+    if not p then
+      print("[goc] no test function found")
+      return
+    end
+  end
+  if p:type() ~= "function_declaration" then
+    p = p:parent()
+    if not p then
+      print("[goc] no test function found")
+      return
+    end
+    return M.CoverageFunc(p, html, customArgs)
+  end
+  return M.Coverage(string.gmatch(ts.get_node_text(p, 1), 'Test[^%s%(]+')(), html, customArgs)
+end
+
+M.ClearCoverage = function(bufnr)
+  vim.api.nvim_buf_clear_namespace(bufnr or 0, M.hi, 0, -1)
+end
+
+M.AlternateFile = function(split, suffix, sp)
+  local path = vim.fn.expand('%:h') .. '/' -- relative
+  local file = vim.fn.expand('%:t:r')
+  local ext = vim.fn.expand('%:e')
+  local aux = suffix .. '.'
+  if string.find(file, suffix) then
+    aux = '.'
+    file = string.gsub(file, suffix, '')
+  end
+
+  local bufnr = vim.fn.bufadd(path .. file .. aux .. ext)
+  if split then
+    vim.cmd(":" .. sp .. " sb " .. path .. file .. aux .. ext)
+  else
+    local isVisible = #vim.fn.win_findbuf(bufnr) ~= 0
+    if isVisible then
+      vim.cmd(":sb " .. path .. file .. aux .. ext)
+    else
+      vim.cmd('e ' .. path .. file .. aux .. ext)
+    end
+  end
+end
+
+M.AlternateSplit = function()
+  M.AlternateFile(true, "_test", M.splitSBCmd)
+end
+
+M.Alternate = function(split)
+  M.AlternateFile(split, "_test", M.splitSBCmd)
+end
+
+return M
