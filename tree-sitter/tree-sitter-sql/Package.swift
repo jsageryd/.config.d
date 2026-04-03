fb76f5604b564f078dc1afb1978312b7971611e1
Johan Sageryd <j@1616.se>
2026-04-03 18:29:16 +0200

    tree-sitter: Add tree-sitter-sql
    From the latest commit of branch gh-pages which has the generated parser
    files, as mentioned by the readme.
    
    Source: https://github.com/DerekStride/tree-sitter-sql
    Commit: 851e9cb257ba7c66cc8c14214a31c44d2f1e954e

---
+++ tree-sitter/tree-sitter-sql/Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TreeSitterSql",
+    products: [
+        .library(name: "TreeSitterSql", targets: ["TreeSitterSql"]),
+    ],
+    dependencies: [
+        .package(name: "SwiftTreeSitter", url: "https://github.com/tree-sitter/swift-tree-sitter", from: "0.8.0"),
+    ],
+    targets: [
+        .target(
+            name: "TreeSitterSql",
+            dependencies: [],
+            path: ".",
+            sources: [
+                "src/parser.c",
+                "src/scanner.c"
+            ],
+            resources: [
+                .copy("queries")
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")]
+        ),
+        .testTarget(
+            name: "TreeSitterSqlTests",
+            dependencies: [
+                "SwiftTreeSitter",
+                "TreeSitterSql",
+            ],
+            path: "bindings/swift/TreeSitterSqlTests"
+        )
+    ],
+    cLanguageStandard: .c11
+)
